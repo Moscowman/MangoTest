e08dea3001db18d608dfde99bc829b2ca5107003
Vadim <vraspopin@inbox.ru>
2024-10-04 15:57:10 +0300

    First iteration of registration screen

---
+++ app/src/main/java/com/vadimraspopin/mangotest/ui/RegistrationScreen.kt
@@ -0,0 +1,104 @@
+package com.vadimraspopin.mangotest.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vadimraspopin.mangotest.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegistrationScreen(phoneNumber: String) {
+    var name by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
+
+    var isLoading by remember { mutableStateOf(false) }
+
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.onBackground,
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(stringResource(R.string.registration_screen_title)) },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        }) { paddingValues ->
+        val configuration = LocalConfiguration.current
+        val screenHeight = configuration.screenHeightDp.dp
+        val offsetY = screenHeight * 0.2f
+
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(24.dp)
+                .fillMaxSize()
+                .offset(y = offsetY),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.registration_screen_phone_number_label) + ": $phoneNumber"
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text(stringResource(R.string.registration_screen_name_textfield_suggestion)) },
+                modifier = Modifier.widthIn(max = 488.dp),
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = username,
+                onValueChange = {
+                    username = it
+                },
+                label = { Text(stringResource(R.string.registration_screen_username_textfield_suggestion)) },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Ascii
+                ),
+                modifier = Modifier.widthIn(max = 488.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            if (isLoading) {
+                CircularProgressIndicator()
+            } else {
+                Button(
+                    onClick = {},
+                    modifier = Modifier.widthIn(max = 320.dp),
+                ) {
+                    Text(stringResource(R.string.registration_screen_register_button_label))
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RegistrationScreenPreview() {
+    RegistrationScreen("+7 123 45-67")
+}
